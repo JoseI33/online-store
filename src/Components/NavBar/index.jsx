bb358e7ebc69efd1d0c10127fdbb301c5f29303c
USUARIO <jose.imhoff.20@gmail.com>
2024-08-19 21:49:06 -0300

    Se sube proyecto

---
+++ src/Components/NavBar/index.jsx
@@ -0,0 +1,79 @@
+import { NavLink } from "react-router-dom"
+const Navbar = () => {
+    return (
+        <nav className="flex justify-between items-center">
+            <ul>
+                <li>
+                    <NavLink
+                        to='/'>
+                        Shopi
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to='/'>
+                        All
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to='/clothes'>
+                        Clothes
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to='/electronics'>
+                        Electronics
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to='/fornitures'>
+                        Fornitures
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to='/toys'>
+                        Toys
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to='/others'>
+                        Others
+                    </NavLink>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    jose.imhoff.20@gmail.com
+                </li>
+                <li>
+                    <NavLink
+                        to='/my-orders'>
+                        My Orders
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to='/my-account'>
+                        My Account
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to='/sign-in'>
+                        Sign In
+                    </NavLink>
+                </li>
+                <li>
+                    Carrito 0
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
