0f9cb468e58434608ef19d562b72925bca327de0
USUARIO <jose.imhoff.20@gmail.com>
2024-08-25 22:10:34 -0300

    creando apis

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,26 @@
+import { useState, useEffect } from "react"
 import Layout from "../../Components/Layout"
+import Card from "../../Components/Card"
 
 function Home() {
-  return (
-    <Layout>
-        Home
-    </Layout>
-  )
+    const [items, setItems] = useState(null)
+
+    useEffect(() => {
+        fetch('https://api.escuelajs.co/api/v1/products')
+            .then(response => response.json())
+            .then(data => setItems(data))
+    }, [])
+
+    return (
+        <Layout>
+            Home
+            {
+                items?.map(item => {
+                  return  <Card key={item.id} data={item} />
+                })
+            }
+        </Layout>
+    )
 }
 
 export default Home
